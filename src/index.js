84e83835dc243f1f0923c0e4ea36f3ca8828c548
easylogic <cyberuls@gmail.com>
2021-06-17 02:23:01 +0900

    refactoring....

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import PathParser from "./util/PathParser";
+import PathStringManager from "./util/PathStringManager";
+import Point from "./util/Point";
 
 // import Util from "./util/index";
+import * as bezier from "./util/bezier";
 
 
 export default {
   version: '@@VERSION@@',
   // ...Util,
+  bezier,
+  Point,
   PathParser,
+  PathStringManager
 };
